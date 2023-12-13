10dc879b211cbb3af0b0e19ecd3fb1348ed1e33e
Zacker1101 <shaikzeeshan896@gmail.com>
2023-12-13 23:49:17 +0530

    Add Codegen and Apollo Client

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,7 +1,17 @@
-export default function Page() {
-    return (
-        <main className="flex min-h-screen w-full flex-col p-10">
-            <h1>Anime</h1>
-        </main>
-    );
+import { gql } from "@/__generated__";
+import { Button } from "@/components/ui/button";
+import { getClient } from "@/lib/graphql";
+import anilist from "@/lib/oauth";
+import { cookies } from "next/headers";
+import Link from "next/link";
+
+export default async function Page() {
+  return (
+    <main className="flex min-h-screen w-full flex-col p-10">
+      <h1>Anime</h1>
+      <Button asChild variant="link">
+        <Link href={anilist.getAuthURL()}>Login</Link>
+      </Button>
+    </main>
+  );
 }
