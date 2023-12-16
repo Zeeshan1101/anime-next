1197f22e7a064e3f07aedae7762e5b15d4b143e7
Zeeshan1101 <shaikzeeshan896@gmail.com>
2023-12-16 22:30:19 +0530

    Responsive Media Card

--- src/components/media/media-card.tsx
+++ src/components/media/media-card.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const MediaCard = ({ image }: { image: MediaFragment }) => {
     return (
-        <div className="card flex min-h-[24rem] w-[16rem] min-w-[16rem] flex-shrink-0 flex-col gap-1">
+        <div className="card flex min-h-[16rem] w-[10rem]  flex-shrink-0 flex-col gap-1 sm:min-h-[18rem]  md:min-h-[20rem] md:w-[12rem] lg:min-h-[22rem]  lg:w-[14rem] xl:min-h-[24rem] xl:w-[16rem]">
             <div className="relative flex-1 overflow-hidden rounded-lg">
                 <Image
                     src={image?.coverImage?.extraLarge as string}
@@ -16,7 +16,7 @@ export const MediaCard = ({ image }: { image: MediaFragment }) => {
                 <div className="absolute inset-0 z-10 bg-black/0" />
             </div>
 
-            <div className="line-clamp-1 h-5 pl-4">
+            <div className="md:text-normal line-clamp-1 h-6 pl-1 text-sm md:pl-4">
                 {image.title?.userPreferred}
             </div>
         </div>
