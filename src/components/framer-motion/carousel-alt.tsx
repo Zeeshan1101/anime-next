7681fec1a40b0de21ded536abbce9f779e4e4f40
Shaik Zeeshan <shaikzeeshan896@gmail.com>
2023-12-18 16:12:33 +0530

    Fix Layout

--- src/components/framer-motion/carousel-alt.tsx
+++ src/components/framer-motion/carousel-alt.tsx
@@ -8,122 +8,114 @@ import moment from "moment";
 import { MediaFragment } from "@/__generated__/graphql";
 
 const CarouselAlt = <T extends MediaFragment[]>({ images }: { images: T }) => {
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const filterImages = images.filter((image) => image?.bannerImage);
+  const filterImages = images.filter((image) => image?.bannerImage);
 
-    useIsomorphicLayoutEffect(() => {
-        const interval = setInterval(() => {
-            setActiveIndex((prev) => (prev + 1) % filterImages.length);
-        }, 3000);
-        return () => clearInterval(interval);
-    }, [images.length, activeIndex]);
+  useIsomorphicLayoutEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prev) => (prev + 1) % filterImages.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length, activeIndex]);
 
-    return (
-        <div className="relative h-96 w-full overflow-hidden rounded-lg">
-            <motion.div
-                className="relative h-full w-full"
-                animate={{ y: activeIndex * -100 + "%" }}
-                transition={{
-                    duration: 0.5,
-                    type: "spring",
-                    bounce: 0.2,
-                    ease: "easeInOut",
-                }}
-            >
-                {filterImages.map((image, index) => {
-                    if (image?.bannerImage === null) return;
-                    return (
-                        <div key={index} className="h-full w-full">
-                            <CarouselItem
-                                image={image?.bannerImage || ""}
-                                alt={image.title?.userPreferred as string}
-                                episode={
-                                    image.nextAiringEpisode?.episode as number
-                                }
-                                airingAt={
-                                    image.nextAiringEpisode?.airingAt as number
-                                }
-                            />
-                        </div>
-                    );
-                })}
-            </motion.div>
-            <div className="absolute right-5 top-1/2 z-[999] -translate-y-1/2">
-                <div className="flex flex-col items-center gap-4">
-                    {filterImages.map((image, index) => {
-                        if (image?.bannerImage === null) return;
-
-                        return (
-                            <motion.button
-                                key={index}
-                                className={cn(
-                                    "relative z-10 h-4 w-4 rounded-full bg-[--media-color] transition-all duration-300",
-                                )}
-                                style={
-                                    {
-                                        "--media-color":
-                                            image?.coverImage?.color ||
-                                            "hsl(var(--background))",
-                                    } as CSSProperties
-                                }
-                                onClick={() => {
-                                    setActiveIndex(index);
-                                }}
-                            >
-                                {activeIndex === index && (
-                                    <motion.span
-                                        layoutId="coursel-on"
-                                        className="absolute inset-0 h-full w-full rounded-full outline outline-offset-2 outline-[--media-color]"
-                                        animate={{
-                                            outlineColor: "var(--media-color)",
-                                        }}
-                                    ></motion.span>
-                                )}
-                            </motion.button>
-                        );
-                    })}
-                </div>
+  return (
+    <div className="relative h-96 w-full overflow-hidden rounded-lg">
+      <motion.div
+        className="relative h-full w-full"
+        animate={{ y: activeIndex * -100 + "%" }}
+        transition={{
+          duration: 0.5,
+          type: "spring",
+          bounce: 0.2,
+          ease: "easeInOut",
+        }}
+      >
+        {filterImages.map((image, index) => {
+          if (image?.bannerImage === null) return;
+          return (
+            <div key={index} className="h-full w-full">
+              <CarouselItem
+                image={image?.bannerImage || ""}
+                alt={image.title?.userPreferred as string}
+                episode={image.nextAiringEpisode?.episode as number}
+                airingAt={image.nextAiringEpisode?.airingAt as number}
+              />
             </div>
+          );
+        })}
+      </motion.div>
+      <div className="absolute right-5 top-1/2 z-[999] -translate-y-1/2">
+        <div className="flex flex-col items-center gap-4">
+          {filterImages.map((image, index) => {
+            if (image?.bannerImage === null) return;
+
+            return (
+              <motion.button
+                key={index}
+                className={cn(
+                  "relative z-10 h-4 w-4 rounded-full bg-[--media-color] transition-all duration-300"
+                )}
+                style={
+                  {
+                    "--media-color":
+                      image?.coverImage?.color || "hsl(var(--background))",
+                  } as CSSProperties
+                }
+                onClick={() => {
+                  setActiveIndex(index);
+                }}
+              >
+                {activeIndex === index && (
+                  <motion.span
+                    layoutId="coursel-on"
+                    className="absolute inset-0 h-full w-full rounded-full outline outline-offset-2 outline-[--media-color]"
+                    animate={{
+                      outlineColor: "var(--media-color)",
+                    }}
+                  ></motion.span>
+                )}
+              </motion.button>
+            );
+          })}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 const CarouselItem = ({
-    image,
-    alt,
-    episode,
-    airingAt,
+  image,
+  alt,
+  episode,
+  airingAt,
 }: {
-    image: string;
-    alt: string;
-    episode: number;
-    airingAt: number;
+  image: string;
+  alt: string;
+  episode: number;
+  airingAt: number;
 }) => {
-    return (
-        <div className="relative z-10 h-full w-full">
-            <Image
-                src={image}
-                fill
-                alt={alt}
-                className="h-full w-full object-cover object-center"
-            />
-            <div className="absolute bottom-5 z-20 flex w-full flex-col items-center justify-between px-14 text-center sm:flex-row sm:text-justify">
-                <div className="text-white">
-                    <div className="w-full text-xl font-semibold sm:text-2xl ">
-                        {alt}
-                    </div>
-                    <div className="opacity-60 ">
-                        {episode && episode} Episodes
-                    </div>
-                </div>
-                <div className="text-normal font-medium text-white sm:text-xl">
-                    {airingAt && moment.unix(airingAt).calendar()}
-                </div>
-            </div>
-            <div className="absolute inset-0 z-10 h-full w-full bg-black opacity-50"></div>
+  return (
+    <div className="relative z-10 h-full w-full">
+      <Image
+        src={image}
+        fill
+        priority={true}
+        alt={alt}
+        className="h-full w-full object-cover object-center"
+      />
+      <div className="absolute bottom-5 z-20 flex w-full flex-col items-center justify-between px-14 text-center sm:flex-row sm:text-justify">
+        <div className="text-white">
+          <div className="w-full text-xl font-semibold sm:text-2xl ">{alt}</div>
+          <div className="opacity-60 ">{episode && episode} Episodes</div>
         </div>
-    );
+        <div className="text-normal font-medium text-white sm:text-xl">
+          {airingAt && moment.unix(airingAt).calendar()}
+        </div>
+      </div>
+      <div className="absolute inset-0 z-10 h-full w-full bg-black opacity-50"></div>
+    </div>
+  );
 };
 
 export { CarouselAlt };
--- src/components/framer-motion/horizontal-scroll.tsx
+++ src/components/framer-motion/horizontal-scroll.tsx
@@ -6,68 +6,68 @@ import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { useGesture } from "@use-gesture/react";
 
 const HorizontalScrollList = ({
-    children,
-    className,
-    width = 0,
-    ...props
+  children,
+  className,
+  width = 0,
+  ...props
 }: {
-    children: ReactNode;
-    className?: string;
-    width?: number;
+  children: ReactNode;
+  className?: string;
+  width?: number;
 }) => {
-    const ref = useRef<any>(null);
+  const ref = useRef<any>(null);
 
-    const hasDragged = useRef(false);
-    const dragControls = useDragControls();
-    const [scroll, setScroll] = useState(0);
+  const hasDragged = useRef(false);
+  const dragControls = useDragControls();
+  const [scroll, setScroll] = useState(0);
 
-    const bind = useGesture(
-        {
-            onDragStart: ({ first, last, event }) => {
-                if (first) hasDragged.current = true;
-                if (last) setTimeout(() => (hasDragged.current = false), 0);
-                dragControls.start(event as PointerEvent);
-            },
-            onClickCapture: (event) => {
-                if (hasDragged.current) {
-                    event.event?.stopPropagation();
-                }
-            },
-        },
-        {
-            drag: {
-                filterTaps: true,
-            },
-        },
-    );
-
-    useEffect(() => {
-        if (ref?.current && !width) {
-            setScroll(ref.current.scrollWidth - ref.current.clientWidth);
-        } else {
-            setScroll(width);
+  const bind = useGesture(
+    {
+      onDragStart: ({ first, last, event }) => {
+        if (first) hasDragged.current = true;
+        if (last) setTimeout(() => (hasDragged.current = false), 0);
+        dragControls.start(event as PointerEvent);
+      },
+      onClickCapture: (event) => {
+        if (hasDragged.current) {
+          event.event?.stopPropagation();
         }
-    }, [width]);
+      },
+    },
+    {
+      drag: {
+        filterTaps: true,
+      },
+    }
+  );
 
-    return (
-        <div ref={ref} className="overflow-hidden">
-            {/* @ts-ignore */}
-            <motion.div
-                data-x={scroll}
-                drag="x"
-                dragConstraints={{
-                    right: 0,
-                    left: -scroll,
-                }}
-                dragControls={dragControls}
-                {...bind()}
-                className={cn("cursor-grab touch-none", className)}
-                {...props}
-            >
-                <div className="w-full cursor-grab">{children}</div>
-            </motion.div>
-        </div>
-    );
+  useEffect(() => {
+    if (ref?.current && !width) {
+      setScroll(ref.current.scrollWidth - ref.current.clientWidth);
+    } else {
+      setScroll(width);
+    }
+  }, [width]);
+
+  return (
+    <div ref={ref} className="overflow-hidden">
+      {/* @ts-ignore */}
+      <motion.div
+        data-x={scroll}
+        drag="x"
+        dragConstraints={{
+          right: 0,
+          left: -scroll,
+        }}
+        dragControls={dragControls}
+        {...bind()}
+        className={cn("cursor-grab touch-none", className)}
+        {...props}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
 };
 
 export default HorizontalScrollList;
