8c879e3b1392c9ec1ee726875109138416a06d98
Zeeshan1101 <shaikzeeshan896@gmail.com>
2023-12-20 00:10:29 +0530

    Add Open Graph Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <head>
-                <meta property="og:image" content="<generated>" />
-                <meta property="og:image:alt" content="About Acme" />
-                <meta property="og:image:type" content="image/png" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-            </head>
             <body className={poppins.className}>
                 <SpeedInsights />
                 <ThemeProvider
