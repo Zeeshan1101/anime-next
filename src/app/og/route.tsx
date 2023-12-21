c063f1840089bad193c33b3137dccd215d418ffb
Zeeshan1101 <shaikzeeshan896@gmail.com>
2023-12-21 09:10:38 +0530

    Testing OG route

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -24,9 +24,6 @@ export async function GET() {
                 </div>
             </div>
         ),
-        {
-            width: 1200,
-            height: 630,
-        },
+        {},
     );
 }
