d12805d7f3c167a07ee34e7a9d8a8952f3a67f42
Zeeshan1101 <shaikzeeshan896@gmail.com>
2024-01-03 21:01:50 +0530

    Add Padding to page

--- src/app/(media)/layout.tsx
+++ src/app/(media)/layout.tsx
@@ -6,7 +6,7 @@ import { ScrollUp } from "@/components/scroll-up";
 
 export default function Layout({ children }: { children?: ReactNode }) {
     return (
-        <div className="flex min-h-screen w-full flex-col py-4 pb-10 [--gap:1rem] [--padding-x:1rem] sm:[--gap:1.5rem] sm:[--padding-x:1.5rem] md:[--gap:2rem] md:[--padding-x:2rem] lg:[--gap:2.5rem] lg:[--padding-x:3rem]">
+        <div className="flex min-h-screen w-full flex-col py-4 pb-20 [--gap:1rem] [--padding-x:1rem] sm:[--gap:1.5rem] sm:[--padding-x:1.5rem] md:pb-10 md:[--gap:2rem] md:[--padding-x:2rem] lg:[--gap:2.5rem] lg:[--padding-x:3rem]">
             <div className="mb-5 grid h-max grid-cols-2 items-center justify-between gap-[--gap] overflow-hidden px-[--padding-x] md:flex">
                 <MediaToggle />
                 <SearchInput />
