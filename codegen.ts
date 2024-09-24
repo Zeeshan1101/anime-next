8c855ce380ec0dfa3e491fed4761c8b0aa347c49
Zeeshan1101 <shaikzeeshan896@gmail.com>
2024-09-24 14:01:09 +0530

    fix: dotenv

--- codegen.ts
+++ codegen.ts
@@ -1,17 +1,20 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
 
 const config: CodegenConfig = {
-      schema: process.env.ANILIST_GRAPHQL as string,
-      documents: ["src/graphql/**/*.tsx", "src/graphql/**/*.ts"],
-      generates: {
-            "./src/__generated__/": {
-                  preset: "client",
-                  presetConfig: {
-                        gqlTagName: "gql",
-                  },
+    schema: process.env.ANILIST_GRAPHQL as string,
+    documents: ["src/graphql/**/*.tsx", "src/graphql/**/*.ts"],
+    generates: {
+        "./src/__generated__/": {
+            preset: "client",
+            presetConfig: {
+                gqlTagName: "gql",
             },
-      },
-      ignoreNoDocuments: true,
+        },
+    },
+    ignoreNoDocuments: true,
 };
 
 export default config;
