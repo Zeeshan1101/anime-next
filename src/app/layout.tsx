c049d2d04308ac0c72bd9fa8652aa9732c932005
Zeeshan1101 <shaikzeeshan896@gmail.com>
2024-09-29 20:35:14 +0530

    feat: add analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,72 +5,78 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { TailwindInticator } from "@/components/tailwind-indicator";
 import { LazyMotionWrapper } from "@/components/lazymotion-wrapper";
+import Script from "next/script";
 
 const poppins = Poppins({
-      subsets: ["latin"],
-      weight: ["400", "600", "500", "700"],
+	subsets: ["latin"],
+	weight: ["400", "600", "500", "700"],
 });
 
 const appURL = new URL(
-      process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL as string}`
-            : `http://localhost:3000`,
+	process.env.VERCEL_URL
+		? `https://${process.env.VERCEL_URL as string}`
+		: `http://localhost:3000`,
 );
 
 export const metadata: Metadata = {
-      metadataBase: appURL,
-      title: "Anime Tracking App",
-      description: "Track your anime progress",
-      openGraph: {
-            type: "website",
-            locale: "en_US",
-            url: appURL.href,
-            title: "Anime Tracking App",
-            description: "Track your anime progress",
-            images: [
-                  {
-                        url: `${appURL}og`,
-                        width: 1200,
-                        height: 630,
-                        alt: "Anime Tracking App",
-                  },
-            ],
-      },
-      twitter: {
-            title: "Anime Tracking App",
-            description: "Track your anime progress",
-            images: [
-                  {
-                        url: `${appURL}og`,
-                        width: 1200,
-                        height: 630,
-                        alt: "Anime Tracking App",
-                  },
-            ],
-      },
+	metadataBase: appURL,
+	title: "Anime Tracking App",
+	description: "Track your anime progress",
+	openGraph: {
+		type: "website",
+		locale: "en_US",
+		url: appURL.href,
+		title: "Anime Tracking App",
+		description: "Track your anime progress",
+		images: [
+			{
+				url: `${appURL}og`,
+				width: 1200,
+				height: 630,
+				alt: "Anime Tracking App",
+			},
+		],
+	},
+	twitter: {
+		title: "Anime Tracking App",
+		description: "Track your anime progress",
+		images: [
+			{
+				url: `${appURL}og`,
+				width: 1200,
+				height: 630,
+				alt: "Anime Tracking App",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
-      children,
+	children,
 }: {
-      children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-      return (
-            <html lang="en" suppressHydrationWarning>
-                  <body className={poppins.className}>
-                        <ThemeProvider
-                              attribute="class"
-                              defaultTheme="system"
-                              enableSystem
-                              disableTransitionOnChange
-                        >
-                              <LazyMotionWrapper>{children}</LazyMotionWrapper>
-                              <div className="fixed bottom-3 right-2 z-50">
-                                    <ThemeToggle />
-                              </div>
-                              <TailwindInticator />
-                        </ThemeProvider>
-                  </body>
-            </html>
-      );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={poppins.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<LazyMotionWrapper>{children}</LazyMotionWrapper>
+					<div className="fixed bottom-3 right-2 z-50">
+						<ThemeToggle />
+					</div>
+					<TailwindInticator />
+				</ThemeProvider>
+				<Script
+					defer
+					src="https://track.shaikzeeshan.me/script.js"
+					data-website-id="f521bcc8-f0c3-43a9-839b-f16698967364"
+				/>
+			</body>
+		</html>
+	);
 }
