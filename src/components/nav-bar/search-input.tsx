29b34c740e6ef2dbedc70f1eb71620a09b1809c4
Zeeshan1101 <shaikzeeshan896@gmail.com>
2024-11-28 13:30:44 +0530

    fix: type error for searchParams

--- src/components/nav-bar/search-input.tsx
+++ src/components/nav-bar/search-input.tsx
@@ -8,76 +8,76 @@ import React from "react";
 import { Button } from "../ui/button";
 
 export const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ className, ...props }, ref) => {
-        const pathname = usePathname();
+	({ className, ...props }, ref) => {
+		const pathname = usePathname();
 
-        const searchParams = useSearchParams();
+		const searchParams = useSearchParams();
 
-        const router = useRouter();
+		const router = useRouter();
 
-        const search = searchParams.get("search");
+		const search = searchParams.get("search");
 
-        const [searchValue, setSearchValue] = React.useState("");
+		const [searchValue, setSearchValue] = React.useState("");
 
-        React.useEffect(() => {
-            setSearchValue(search || "");
-        }, [search]);
+		React.useEffect(() => {
+			setSearchValue(search || "");
+		}, [search]);
 
-        const handleSearch = React.useCallback(() => {
-            let path = pathname.startsWith("/anime") ? "/anime" : "/manga";
+		const handleSearch = React.useCallback(() => {
+			let path = pathname.startsWith("/anime") ? "/anime" : "/manga";
 
-            const params = new URLSearchParams(searchParams);
+			const params = new URLSearchParams(searchParams.toString());
 
-            if (searchValue) {
-                params.set("search", searchValue);
-            } else {
-                params.delete("search");
-            }
+			if (searchValue) {
+				params.set("search", searchValue);
+			} else {
+				params.delete("search");
+			}
 
-            const paramString = params.toString().length
-                ? `?${params.toString()}`
-                : "";
+			const paramString = params.toString().length
+				? `?${params.toString()}`
+				: "";
 
-            path = `${path}/filter${paramString}`;
-            return path;
-        }, [pathname, searchValue, searchParams]);
+			path = `${path}/filter${paramString}`;
+			return path;
+		}, [pathname, searchValue, searchParams]);
 
-        return (
-            <div
-                className={cn(
-                    "relative order-3 col-span-2 w-full  md:order-2 lg:w-96",
-                    className,
-                )}
-            >
-                <Input
-                    ref={ref}
-                    {...props}
-                    className="h-full w-full pl-5"
-                    placeholder="Search..."
-                    onChange={(e) => {
-                        setSearchValue(e.target.value);
-                    }}
-                    value={searchValue}
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            const path = handleSearch();
-                            router.push(path);
-                        }
-                    }}
-                />
-                <Button
-                    className="absolute right-0 top-1/2 -translate-y-1/2"
-                    variant="ghost"
-                    onClick={() => {
-                        const path = handleSearch();
-                        router.push(path);
-                    }}
-                >
-                    <SlidersHorizontal className="h-4 w-4" />
-                </Button>
-            </div>
-        );
-    },
+		return (
+			<div
+				className={cn(
+					"relative order-3 col-span-2 w-full  md:order-2 lg:w-96",
+					className,
+				)}
+			>
+				<Input
+					ref={ref}
+					{...props}
+					className="h-full w-full pl-5"
+					placeholder="Search..."
+					onChange={(e) => {
+						setSearchValue(e.target.value);
+					}}
+					value={searchValue}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							const path = handleSearch();
+							router.push(path);
+						}
+					}}
+				/>
+				<Button
+					className="absolute right-0 top-1/2 -translate-y-1/2"
+					variant="ghost"
+					onClick={() => {
+						const path = handleSearch();
+						router.push(path);
+					}}
+				>
+					<SlidersHorizontal className="h-4 w-4" />
+				</Button>
+			</div>
+		);
+	},
 );
 
 SearchInput.displayName = "SearchInput";
